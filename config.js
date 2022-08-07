fbad3dd91df5163f7ead77db857500736877799b
Lukas <36674091+Swepool@users.noreply.github.com>
2022-08-07 13:14:15 +0200

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+export const config = {
+    WEBSOCKET_URL: 'wss://cache.hugin.chat',
+    NODE: 'blocksum.org',
+    PORT: '11898',
+    CHANNEL_ID: '1005512632855445654',
+    BOT_ID:'1005518787484856400',
+    DEFAULT_NICKNAME: 'Anon',
+    BOT_ADDRESS: 'SEKReX8rtdrG3S97voALZcfYKbguCwDi79nNpYXsn6DTFyrA7DGRumQSNDuMRuaJY84C8TGZc2vyY9mMiN28Ve7ebULj7iuqr1B'
+}
