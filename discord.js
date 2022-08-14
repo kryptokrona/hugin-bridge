36dcb2ec76a1886f3023389fb72579ec38744f63
Lukas <36674091+Swepool@users.noreply.github.com>
2022-08-14 13:05:48 +0200

    Update discord.js

--- discord.js
+++ discord.js
@@ -19,13 +19,13 @@ export const startDiscord = () => {
         console.log('The bot is ready 🥳')
     })
 
-    client.login(process.env.TOKEN)
+    client.login(process.env.DISCORD_BOT_TOKEN)
         .then(() => {
             console.log('Logged into Discord')
         })
 
     client.on('messageCreate', data => {
-        if (data.channelId === config.CHANNEL_ID && data.author.id !== config.BOT_ID) {
+        if (data.channelId === config.DISCORD_CHANNEL_ID && data.author.id !== config.DISCORD_BOT_ID) {
             sendHuginMessage(data.author.username, (data.content))
         }
     })
@@ -35,7 +35,7 @@ export const startDiscord = () => {
 //Sends message to set CHANNEL_ID
 export const sendDiscordMessage = (nickname, message) => {
     message = he.decode(message)
-    let channel = client.channels.cache.get(config.CHANNEL_ID)
+    let channel = client.channels.cache.get(config.DISCORD_CHANNEL_ID)
     if (nickname == null) nickname = config.DEFAULT_NICKNAME
     channel.send(`${nickname}: ${message}`)
     console.log(`Sent Discord message`)
