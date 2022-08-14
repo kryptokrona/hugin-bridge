9e18858ce08a539c76370bee4d76b14256e8ac94
Lukas <36674091+Swepool@users.noreply.github.com>
2022-08-14 13:05:50 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 import {startWebsocket} from "./websocket.js";
 import {startDiscord} from "./discord.js";
 import {startHugin} from "./hugin.js";
+import {startTelegram} from "./telegram.js";
+
 
 startWebsocket()
-startDiscord()
 startHugin()
+startTelegram()
+startDiscord()
