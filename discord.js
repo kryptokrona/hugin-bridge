df5930d731caeb9a6b977b8e0715797639447ad2
Lukas <36674091+Swepool@users.noreply.github.com>
2022-08-15 22:55:36 +0200

    Forgot to add board to message

--- discord.js
+++ discord.js
@@ -37,7 +37,7 @@ export const sendDiscordMessage = (nickname, message, board) => {
     message = he.decode(message)
     let channel = client.channels.cache.get(config.DISCORD_CHANNEL_ID)
     if (nickname == null) nickname = config.DEFAULT_NICKNAME
-    channel.send(`${nickname}: ${message}`)
+    channel.send(`${board} | ${nickname}: ${message}`)
     console.log(`Sent Discord message`)
     console.log(`${board} | ${nickname}: ${message}`)
 }
