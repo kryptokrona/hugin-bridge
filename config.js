cadc957a54f74aefe190a2bc7cea472a82440262
Lukas <36674091+Swepool@users.noreply.github.com>
2022-08-25 14:45:05 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 export const config = {
     WEBSOCKET_URL: 'wss://cache.hugin.chat',
-    NODE: 'blocksum.org',
+    NODE: 'localhost',
     PORT: '11898',
     DISCORD_CHANNEL_ID: '1005512632855445654',
     DISCORD_BOT_ID:'1005518787484856400',
     DEFAULT_NICKNAME: 'Anon',
-    BOT_ADDRESS: 'SEKReX8rtdrG3S97voALZcfYKbguCwDi79nNpYXsn6DTFyrA7DGRumQSNDuMRuaJY84C8TGZc2vyY9mMiN28Ve7ebULj7iuqr1B',
+    BOT_ADDRESS: 'enter address',
     TELEGRAM_GROUP_ID: '-1001354729641'
 }
